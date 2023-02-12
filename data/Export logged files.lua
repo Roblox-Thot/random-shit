d6270bee3ef21869046163c2874ed289d4a250a5
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2023-02-11 20:01:30 -0500

    Update Export logged files.lua

--- data/Export logged files.lua
+++ data/Export logged files.lua
@@ -1,13 +1,17 @@
 local logFileName = "out.txt"
+local outFolder = "export"
 
 --------------------------------------
+
+-- make output folder if not there
+if not isfolder(outFolder) then makefolder(outFolder) end
 
 local fileData = readfile(logFileName)
 local jsonData = Game:GetService("HttpService"):JSONDecode(fileData)
 for name, data in pairs(jsonData) do
     local splitName = name:split("\\")
     if #splitName > 1 then
-        folderPath = ""
+        folderPath = outFolder.."/"
         for i = 1, #splitName - 1 do
             folderPath = folderPath .. splitName[i].."/"
             if not isfolder(splitName[i]) then
@@ -15,5 +19,5 @@ for name, data in pairs(jsonData) do
             end
         end
     end
-    writefile(name,data)
+    writefile(outFolder.."/"..name,data)
 end
