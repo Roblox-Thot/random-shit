ea3dfdb4288c42a12ebd2e72508ffbaf3e184fe5
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2023-11-12 14:29:43 -0800

    Create badges.lua

---
+++ badges.lua
@@ -0,0 +1,15 @@
+-- Prints all badge IDs for games
+
+local HttpService = game:GetService("HttpService")
+local badgeUrl = "https://badges.roblox.com/v1/universes/"..game.GameId.."/badges?limit=100&sortOrder=Asc&cursor="
+local cursor = ""
+
+repeat
+   local response = game:HttpGet(badgeUrl..cursor)
+   local data = HttpService:JSONDecode(response)
+   cursor = data.nextPageCursor
+   
+   for _, badge in ipairs(data.data) do
+       print(badge.id)
+   end
+until not cursor
