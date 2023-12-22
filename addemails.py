7a8ce0afb184f4ace8488c7914cb42ea376d09e9
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2023-12-22 10:15:23 -0800

    Create addemails.py

---
+++ addemails.py
@@ -0,0 +1,35 @@
+'''
+You will need a "catch all" on a domain and forward it to an email you have access to.
+Just change "DOMAIN" to the domain that it is set up for.
+'''
+import requests, secrets, string
+from time import sleep
+
+class Shit:
+    def __init__(self, cookie:str, host:str):
+        self.cookie = cookie
+        self.host = host
+        self.request = requests.Session()
+        self.request.cookies.update({'.ROBLOSECURITY':self.cookie})
+        pass
+
+    def email(self): # Random email
+        chars = string.ascii_lowercase + string.digits
+        user = ''.join(secrets.choice(chars) for _ in range(15))
+        return f'{user}@{self.host}'
+
+    def getXsrf(self):
+        xsrHeader = self.request.post("https://auth.roblox.com/v2/login", headers={
+            "X-CSRF-TOKEN": ""
+        }).headers['x-csrf-token']
+        return xsrHeader
+    
+    def emailTH(self):
+        data = {"emailAddress":self.email(), "password":""}
+        req = self.request.post('https://accountsettings.roblox.com/v1/email',json=data,headers={'x-csrf-token':self.getXsrf()})
+        return req
+
+for i in open('cookies.txt','r').readlines():
+    a = Shit(i.strip()
+        ,"DOMAIN").emailTH()
+    print(a,a.text)
