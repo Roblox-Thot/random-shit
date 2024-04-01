2366168c6ea2d4baa43befde8be96a7bca9ec3ea
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2024-03-31 18:01:01 -0700

    Update RobloxUnwarn.py

--- data/RobloxUnwarn.py
+++ data/RobloxUnwarn.py
@@ -1,22 +1,40 @@
 '''
 This is the code that can be used to reactivate a Roblox account after a ban/warn
 '''
-
-from bs4 import BeautifulSoup
+import time
 from requests import session
 
-sus = session()
+def unwarn(cookie:str):
+    requests = session()
+    requests.cookies.update({'.ROBLOSECURITY': cookie})
+    Token = requests.post('https://auth.roblox.com/v1/login').headers['x-csrf-token'] # Grabs the x-csrf token
+    headers = {
+                'content-type': 'application/json',
+                'X-Csrf-Token': Token
+            }
+    
+    ban_data = requests.get("https://usermoderation.roblox.com/v1/not-approved/", headers= headers) 
+    while True:
+        try:
+            if ban_data.status_code == 200:
+                ban_data = ban_data.json()
+                if len(ban_data) > 0:
+                    punishmentTypeDescription = ban_data["punishmentTypeDescription"]
+                    print(f'The account has been {punishmentTypeDescription}')
+                    if punishmentTypeDescription != "Warn": break
 
-cookie = "ROBLOX COOKIE"
+                    attempt = requests.post("https://usermoderation.roblox.com/v1/not-approved/reactivate", headers= headers)
+                    if attempt.status_code == 200:
+                        print("The account has been reactivated") 
+                        break
 
-sus.cookies.update({'.ROBLOSECURITY':cookie})
+                else:
+                    print("The account is in good standing")
+                    break
+        except Exception as e:
+            print(f"Reactivation error: {e}")
+            time.sleep(1)
 
-html_input = sus.get("https://www.roblox.com/not-approved").content # Get warn data
-
-# Get the form data to post
-soup = BeautifulSoup(html_input, 'html.parser')
-verification_token = soup.find('input', {'name': '__RequestVerificationToken'})['value']
-punishmentId = soup.find('input', {'name': 'punishmentId'})['value']
-
-# Post the data
-req = sus.post("https://www.roblox.com/not-approved/reactivate", data={'__RequestVerificationToken':verification_token,'punishmentId':punishmentId}, headers={'Content-Type':'application/x-www-form-urlencoded'})
+if __name__ == '__main__':
+    cookie = input("Cookie: ")
+    unwarn(cookie)
