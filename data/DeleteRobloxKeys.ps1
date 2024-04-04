b0fc31eef100c0f50e9bf36fa0a9ad390f99f681
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2024-04-04 01:33:21 -0700

    Update DeleteRobloxKeys.ps1

--- data/DeleteRobloxKeys.ps1
+++ data/DeleteRobloxKeys.ps1
@@ -1,7 +1,8 @@
-# Deletes all Roblox credentials from
-# "Control Panel\User Accounts\Credential Manager"'s windows credential manager
+# Cleans Roblox studio data
 
 # Just run this in a powershell window
+# to delete all Roblox credentials from
+# "Control Panel\User Accounts\Credential Manager"'s windows credential manager
 cmdkey /list | ForEach-Object { if ($_ -like "*roblox*") { cmdkey /del:($_ -replace '^[^=]+',"" -replace "=","") } }
 
 # This clears the Roblox Studio Registry which will clear any other logged in accounts
