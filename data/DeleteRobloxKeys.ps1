f52d1f2550a36887d4940d185cdfcfc34762156a
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2024-04-04 01:35:08 -0700

    Update DeleteRobloxKeys.ps1

--- data/DeleteRobloxKeys.ps1
+++ data/DeleteRobloxKeys.ps1
@@ -6,3 +6,6 @@ cmdkey /list | ForEach-Object { if ($_ -like "*roblox*") { cmdkey /del:($_ -repl
 
 # This clears the Roblox Studio Registry which will clear any other logged in accounts
 reg delete HKCU\Software\Roblox\RobloxStudio /f
+
+# If you rather just clear the users and not the whole Registry you can just do this
+reg delete HKCU\Software\Roblox\RobloxStudio\LoggedInUsersStore /f
