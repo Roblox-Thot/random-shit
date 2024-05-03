8ce86d467e451df4411c847910be7ae2a9504d62
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2024-05-02 21:51:59 -0700

    fix RobloxUnwarn.py

--- data/RobloxUnwarn.py
+++ data/RobloxUnwarn.py
@@ -1,8 +1,5 @@
 '''
 This is the code that can be used to reactivate a Roblox account after a ban/warn
-
-BROKEN?
-
 '''
 import time
 from requests import session
@@ -10,30 +7,32 @@ from requests import session
 def unwarn(cookie:str):
     requests = session()
     requests.cookies.update({'.ROBLOSECURITY': cookie})
-    token = requests.post('https://usermoderation.roblox.com/v1/not-approved/').headers['x-csrf-token'] # Grabs the x-csrf token
+    token = requests.post('https://usermoderation.roblox.com/v1/not-approved/reactivate').headers['x-csrf-token'] # Grabs the x-csrf token
     headers = {
                 'content-type': 'application/json',
                 'X-Csrf-Token': token
             }
     
-    ban_data = requests.get("https://usermoderation.roblox.com/v1/not-approved/", headers= headers) 
+    ban_data = requests.get("https://usermoderation.roblox.com/v1/not-approved", headers= headers) 
     while True:
         try:
-            if ban_data.status_code == 200:
-                ban_data = ban_data.json()
-                if len(ban_data) > 0:
-                    punishmentTypeDescription = ban_data["punishmentTypeDescription"]
-                    print(f'The account has been {punishmentTypeDescription}')
-                    if punishmentTypeDescription != "Warn": break # TODO: check if the unban date has passed
+            if ban_data.status_code != 200: print("Error getting ban data");exit()
+            
+            ban_data = ban_data.json()
+            if len(ban_data) > 0:
+                punishmentTypeDescription = ban_data["punishmentTypeDescription"]
+                print(f'The account has been {punishmentTypeDescription}')
+                if punishmentTypeDescription != "Warn": break # TODO: check if the unban date has passed
 
-                    attempt = requests.post("https://usermoderation.roblox.com/v1/not-approved/reactivate", headers= headers)
-                    if attempt.status_code == 200:
-                        print("The account has been reactivated") 
-                        break
-
-                else:
-                    print("The account is in good standing")
+                attempt = requests.post("https://usermoderation.roblox.com/v1/not-approved/reactivate", headers= headers)
+                if attempt.status_code == 200:
+                    print("The account has been reactivated") 
                     break
+
+            else:
+                print("The account is in good standing")
+                break
+
         except Exception as e:
             print(f"Reactivation error: {e}")
             time.sleep(1)
