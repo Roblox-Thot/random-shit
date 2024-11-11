a1fe10a15289ff01593c70062267830248f4b9f1
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2024-11-11 15:24:05 -0800

    Delete Data/Others/Vencord.bat

--- Data/Others/Vencord.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set "url=https://github.com/Vencord/Installer/releases/latest/download/VencordInstallerCli.exe"
-set "downloadPath=%TEMP%\VencordInstallerCli.exe"
-bitsadmin /transfer VencordInstaller /download /priority normal %url% %downloadPath%
-cls
-%downloadPath%
-del /f /q "%downloadPath%"
-@echo on
