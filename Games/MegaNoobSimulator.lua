47f23843fe40c9f71018722459806544ff108574
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2025-01-28 09:42:32 -0500

    readd Mega Noob Simulator.lua and fix it

---
+++ Games/MegaNoobSimulator.lua
@@ -0,0 +1,20 @@
+--[[
+  Basic auto farm but has a lot of bugs
+  https://www.roblox.com/games/4543144283/NEW-ITEMS-Mega-Noob-Simulator
+]]--
+
+local Character = game.Players.LocalPlayer.Character
+local Punch = game:GetService("ReplicatedStorage").Remotes.Punch
+
+Character.HumanoidRootPart.Anchored = true
+while task.wait() do 
+    theNoob = workspace.Areas["Main Island"].EnemyHolder.Bacon
+
+    Character.HumanoidRootPart.CFrame  = theNoob.Head.CFrame + Vector3.new(-5,10,0)
+    Punch:FireServer(theNoob)
+
+    -- Removes the noob if its dead to skip it
+    if theNoob.Humanoid.Health == 0 then
+        theNoob:Destroy()
+    end
+end
