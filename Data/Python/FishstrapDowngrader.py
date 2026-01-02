07b712e51164f6c87c292c3f37abc4ffa25bd765
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2026-01-02 12:55:59 -0500

    Enhance FishstrapDowngrader with new features
    Added error handling and support for exploit version.

--- Data/Python/FishstrapDowngrader.py
+++ Data/Python/FishstrapDowngrader.py
@@ -1,33 +1,53 @@
 import requests
 
+DESIRED_SPLOIT = 'Volt'
 FISHSTRAP_APP = __import__('os').path.expandvars('%LOCALAPPDATA%/Fishstrap/Fishstrap.exe')
 
 def Print(msg):
     print(f'[Fishstrap Updater] {msg}')
 
 def get_roversion(rotype = 'current'):
-    # TODO: Rewrite as urllib.request that way requests doesn't need installed (who tf doeeve reve requests)
-    url = f'https://inject.today/api/versions/{rotype}'
+    try:
+        url = f'https://inject.today/api/versions/{rotype}'
+        response = requests.get(url).json()
+        return response['Windows']['Version']
+    except Exception as e:
+        return (f'Error fetching {rotype} version: {e}')
+
+def get_weao_rover():
+    url = f'https://weao.gg/api/status/exploits/{DESIRED_SPLOIT.lower()}'
     response = requests.get(url).json()
-    return response['Windows']['Version']
+    return response['data']['rbxversion']
 
 current_version = get_roversion('current')
 previous_version = get_roversion('previous')
+exploit_Rversion = get_weao_rover()
 
 print('------------------------')
 Print(f'Current Version: {current_version}')
 Print(f'Previous Version: {previous_version}')
+Print(f'{DESIRED_SPLOIT} Roblox Version: {exploit_Rversion}')
 print()
 
 print('1. Update to Current')
 print('2. Rollback to Previous')
-print('3. Custom version')
+print(f'3. Update to {DESIRED_SPLOIT} Supported Version')
+print('4. Custom version')
 user_input = input('> ')
 
-set_to_version = previous_version if user_input.strip() == '2' else current_version
 
-if user_input.strip() == '3':
-    custom_version = input('Enter custom version: ')
-    set_to_version = custom_version.strip()
+match user_input.strip():
+    case '1':
+        set_to_version = current_version
+    case '2':
+        set_to_version = previous_version
+    case '3':
+        set_to_version = exploit_Rversion
+    case '4':
+        custom_version = input('Enter custom version: ')
+        set_to_version = custom_version.strip()
+    case _:
+        Print('Invalid option selected. Exiting.')
+        exit(1)
 
 __import__('subprocess').Popen([FISHSTRAP_APP, str(set_to_version)])
